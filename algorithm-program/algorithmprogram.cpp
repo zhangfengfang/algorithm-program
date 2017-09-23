c77af4d2c6ddcb8a45659711ffa7674a785cbdc8
K650D\lvzhaobing <lvzaobing0920@outlook.com>
2017-09-23 15:31:26 +0800

    顺序查找

--- algorithm-program/algorithmprogram.cpp
+++ algorithm-program/algorithmprogram.cpp
@@ -1,19 +1,22 @@
-// 引用参数.cpp: 定义控制台应用程序的入口点。
-//
-
+//算法测试
 #include "stdafx.h"
 #include "iostream"
 using namespace std;
 template <class T>
 
-T Abc(T& a, T& b, T& c) {
-	return a + b + c + b*c + (a + b + c) / (a + b) + 4;
+int  seqsearch(T  a[],const T& x, int n)
+{
+	int i;
+	for (i = 0; i < n && a[i] != x; i++);
+	if (i == n) return -1;
+	return i;
 }
 int main()
 {
-	int a = 1, b = 2, c = 3;
-
-	cout << Abc(a, b, c) << endl;
+	int a[] = {1,2,3,4,5,6,7,8,9};
+	int x = 10;
+	
+	cout << seqsearch(a,x,sizeof(a)) << endl;
 	system("pause");
 	return 0;
 }
