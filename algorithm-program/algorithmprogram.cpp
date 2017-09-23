2948c591bb4f17e7c2cd35405fd7d20b6ab349a0
K650D\lvzhaobing <lvzaobing0920@outlook.com>
2017-09-23 16:21:28 +0800

    累加

--- algorithm-program/algorithmprogram.cpp
+++ algorithm-program/algorithmprogram.cpp
@@ -3,20 +3,21 @@
 #include "iostream"
 using namespace std;
 template <class T>
-
-int  seqsearch(T  a[],const T& x, int n)
+T sum(T a[], int n)
 {
 	int i;
-	for (i = 0; i < n && a[i] != x; i++);
-	if (i == n) return -1;
-	return i;
+	T tsum = 0;
+	for (i = 0; i < n; i++)
+	{
+		tsum += a[i];
+		
+	}
+	return tsum;
 }
 int main()
 {
 	int a[] = {1,2,3,4,5,6,7,8,9};
-	int x = 10;
-	
-	cout << seqsearch(a,x,sizeof(a)) << endl;
+	cout << sum(a,size(a))<< endl;
 	system("pause");
 	return 0;
 }
